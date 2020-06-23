68df070bf2cf765846c016e18addb6391af65dda
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-23 04:44:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-    elsif string == "I LOVE YOU GRANDMA!"
+    if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
